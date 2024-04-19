0cf2b61ff66fab95d95dda6459b8c4e1c5b45e4a
Mike Oliveira <mikeoliveira@outlook.com>
2024-04-19 15:01:36 -0300

    test(localstorage): adicionado teste unitarios para os metodos do servico localstorage

--- src/core/services/local-storage.service.spec.ts
+++ src/core/services/local-storage.service.spec.ts
@@ -13,4 +13,21 @@ describe('LocalStorageService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it(`#${LocalStorageService.prototype.setItem.name} and ${LocalStorageService.prototype.getItem.name} -
+  should return the object equals the let data = {teste1: 'value1', teste2: 'value2'}`, () => {
+    let data = {teste1: 'value1', teste2: 'value2'}
+    service.setItem('teste', data);
+    expect(service.getItem('teste')).toEqual({teste1: 'value1', teste2: 'value2'})
+  })
+
+  it(`#${LocalStorageService.prototype.removeItem.name}
+  should return (null) toBeFalsy - We setItem('teste2') follow remove this key`, () => {
+    let data = {teste3: 'value3', teste4: 'value4'}
+    service.setItem('teste2', data);
+    expect(service.getItem('teste2')).toEqual({teste3: 'value3', teste4: 'value4'});
+    service.removeItem('teste2');
+    expect(service.getItem('teste2')).toBeFalsy;
+  })
 });
+
